5734135e4cf7acb84d6ceb69ad7dab6122772f22
ateilans <artis.teilans@ru.lv>
2015-10-13 15:24:28 +0300

    Artis test

--- app/src/main/java/app/z0nen/Service/DataRequestCompletedEvent.java
+++ app/src/main/java/app/z0nen/Service/DataRequestCompletedEvent.java
@@ -2,7 +2,7 @@ import app.z0nen.Service.HttpRequestCompletedEvent;
 import app.z0nen.Service.StoreRequest;
 
 /**
- * Created by arniskl on 30.12.2015
+ * Created by artist on 30.12.2015
  */
 public final class DataRequestCompletedEvent extends HttpRequestCompletedEvent {
     public DataRequestCompletedEvent(StoreRequest storeRequest) {
