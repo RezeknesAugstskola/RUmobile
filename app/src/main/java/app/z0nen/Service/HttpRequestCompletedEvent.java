e52a527a928c81eec3aafda33660e1096f75f7f1
imants104 <imants104@gmail.com>
2015-10-13 14:20:52 +0300

    Imanta izmaiņas

--- app/src/main/java/app/z0nen/Service/HttpRequestCompletedEvent.java
+++ app/src/main/java/app/z0nen/Service/HttpRequestCompletedEvent.java
@@ -3,7 +3,8 @@ package app.z0nen.Service;
 import schopp.roland.allroundrobot.app.Entities.StoreRequest;
 
 /**
- * Created by arniskl on 30.12.2015 5555..
+ * Created by arniskl on 30.12.2015 5555
+ * asdasdasdasdasdassdasdasdaasdadas..
  */
 public abstract class HttpRequestCompletedEvent {
     private StoreRequest storeRequest;
