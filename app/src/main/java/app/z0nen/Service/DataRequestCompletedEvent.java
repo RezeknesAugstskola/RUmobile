7b9c43dab5d9c8c0fcc97706fb44e44816fb8627
imants104 <imants104@gmail.com>
2015-11-15 15:04:32 +0200

    imants213

--- app/src/main/java/app/z0nen/Service/DataRequestCompletedEvent.java
+++ app/src/main/java/app/z0nen/Service/DataRequestCompletedEvent.java
@@ -2,7 +2,7 @@ import app.z0nen.Service.HttpRequestCompletedEvent;
 import app.z0nen.Service.StoreRequest;
 
 /**
- * Created by imantssss on 30.12.2015
+ * Created by imants123456 on 30.12.2015
  * qwqweqweW
  */
 public final class DataRequestCompletedEvent extends HttpRequestCompletedEvent {
