f6fa7dc0edb482ceaaf4f40b6a1c1256b7f33072
imants104 <imants104@gmail.com>
2015-12-01 14:42:58 +0200

    WORK

--- app/src/main/java/app/z0nen/Service/HttpRequestCompletedEvent.java
+++ app/src/main/java/app/z0nen/Service/HttpRequestCompletedEvent.java
@@ -1,7 +1,5 @@
 package app.z0nen.Service;
 
-import schopp.roland.allroundrobot.app.Entities.StoreRequest;
-
 /**
  * Created by imantsreieee4niks on 30.12.2015 5555
  * asdasdasdasdasdassdasdas2daasdadas..
